a8659ad3630437c81a6e1946dcd1bfb96164e5d7
Gabriel Hamasaki <gabrielhamasaki82@outlook.com>
2024-02-28 00:07:32 -0300

    fix prod middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const session = request.cookies.get('authjs.session-token')
+  const session = request.cookies.get('authjs.session-token') ?? request.cookies.get('__Secure-authjs.session-token');
   if (!session && !request.nextUrl.pathname.startsWith('/login')) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
