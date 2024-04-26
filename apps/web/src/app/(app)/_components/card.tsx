3cda89bac9837b92ee8b835277207323fa24e076
Gabriel Hamasaki <gabrielhamasaki82@outlook.com>
2024-04-25 22:31:48 -0300

    Update dashboard home

---
+++ apps/web/src/app/(app)/_components/card.tsx
@@ -0,0 +1,31 @@
+import { Card as CardRoot, CardContent, CardHeader, CardTitle } from "@atena/ui/card";
+import type { LucideIcon } from "lucide-react";
+
+interface CardProps {
+  title: string;
+  value: number;
+  percentage: string;
+  Icon: LucideIcon;
+}
+
+export default function Card({ title, value, Icon, percentage }: CardProps) {
+  return (
+    <CardRoot>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-medium">
+          {title}
+        </CardTitle>
+        <Icon className="h-4 w-4 text-muted-foreground" />
+      </CardHeader>
+      <CardContent>
+        <div className="text-2xl font-bold">{value.toLocaleString('en-US', {
+          style: 'currency',
+          currency: 'USD'
+        })}</div>
+        <p className="text-xs text-green-500">
+          +{percentage}%
+        </p>
+      </CardContent>
+    </CardRoot>
+  )
+}
--- apps/web/src/app/(app)/page.tsx
+++ apps/web/src/app/(app)/page.tsx
@@ -1,17 +1,60 @@
-import type { Metadata } from "next";
-export const metadata: Metadata = {
-	title: "Summary",
-};
+import {
+	Activity,
+	CreditCard,
+	DollarSign,
+	Users,
+} from "lucide-react"
 
-export default async function HomePage() {
+import Card from "./_components/card"
+import RecentSales from "./_components/recent-sales"
+import Transactions from "./_components/transactions";
+
+export default function Dashboard() {
+	const cards = [
+		{
+			title: "Total Sales",
+			value: 9999,
+			percentage: "25",
+			Icon: DollarSign,
+		},
+		{
+			title: "Total Customers",
+			value: 1000,
+			percentage: "10",
+			Icon: Users,
+		},
+		{
+			title: "Total Orders",
+			value: 100,
+			percentage: "5",
+			Icon: Activity,
+		},
+		{
+			title: "Total Revenue",
+			value: 100000,
+			percentage: "50",
+			Icon: CreditCard,
+		},
+	];
 	return (
-		<>
-			<h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-			<div className="grid grid-cols-6 gap-4">
-				<div className="col-span-2">
-					<span>Hello World</span>
+		<div className="flex min-h-screen w-full flex-col">
+			<main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+				<div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+					{cards.map((card, index) => (
+						<Card
+							key={index}
+							title={card.title}
+							value={card.value}
+							percentage={card.percentage}
+							Icon={card.Icon}
+						/>
+					))}
 				</div>
-			</div>
-		</>
-	);
+				<div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
+					<Transactions />
+					<RecentSales />
+				</div>
+			</main>
+		</div>
+	)
 }
