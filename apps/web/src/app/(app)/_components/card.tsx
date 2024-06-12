481c44538192041eb09058a72f0549db66effa04
Gabriel Hamasaki <gabriel.hamasaki@exata.it>
2024-06-12 14:18:42 -0300

    feat: add docker-composer and update .env.example db user and password default values

--- apps/web/src/app/(app)/_components/card.tsx
+++ apps/web/src/app/(app)/_components/card.tsx
@@ -1,5 +1,11 @@
-import { Card as CardRoot, CardContent, CardHeader, CardTitle } from "@atena/ui/card";
 import type { LucideIcon } from "lucide-react";
+
+import {
+  CardContent,
+  CardHeader,
+  Card as CardRoot,
+  CardTitle,
+} from "@atena/ui/card";
 
 interface CardProps {
   title: string;
@@ -9,24 +15,31 @@ interface CardProps {
   isMoney?: boolean;
 }
 
-export default function Card({ title, value, Icon, percentage, isMoney }: CardProps) {
+export default function Card({
+  title,
+  value,
+  Icon,
+  percentage,
+  isMoney,
+}: CardProps) {
   return (
     <CardRoot>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-sm font-medium">
-          {title}
-        </CardTitle>
+        <CardTitle className="text-sm font-medium">{title}</CardTitle>
         <Icon className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent>
-        <div className="text-2xl font-bold">{isMoney ? value.toLocaleString('en-US', {
-          style: 'currency',
-          currency: 'USD'
-        }) : value}</div>
-        <p className="text-xs text-green-500">
-          +{percentage}%
-        </p>
+        <div className="text-2xl font-bold">
+          {isMoney
+            ? value.toLocaleString("en-US", {
+                style: "currency",
+                currency: "USD",
+              })
+            : value}
+        </div>
+        <p className="text-xs text-green-500">+{percentage}%</p>
       </CardContent>
     </CardRoot>
-  )
+  );
 }
+
